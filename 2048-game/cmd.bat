bae636e6e66d07206186d3b8cd1e92baab361ac0
Eugene <cullofdute19762005@gmail.com>
2024-10-11 14:07:22 +0300

    add compose.yaml

--- 2048-game/cmd.bat
+++ 2048-game/cmd.bat
@@ -4,7 +4,7 @@ docker build -t 2048-game .
 
 docker run -d -p 8080:80 --name container1 2048-game
 
-docker run -d -p 8081:80 --name container2 2048-game
+docker run -d -p 8082:80 --name container2 2048-game
 
 docker ps
 
