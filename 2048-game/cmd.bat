c9ba146dfd6fad0997204b0bd5cacaba231483dd
Eugene <cullofdute19762005@gmail.com>
2024-10-12 13:30:03 +0300

    add 2 container

--- 2048-game/cmd.bat
+++ 2048-game/cmd.bat
@@ -1,10 +1,10 @@
 cd 2048-game
 
-docker build -t 2048-game .
+docker build -t 2048-game:v1 .
 
-docker run -d -p 8080:80 --name container1 2048-game
+docker run -d -p 8080:80 --name container1 2048-game:v1
 
-docker run -d -p 8082:80 --name container2 2048-game
+docker run -d -p 8082:80 --name container2 2048-game:v1
 
 docker ps
 
